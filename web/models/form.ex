68845e1732a02a4d4332b766e23beb0f505bacb2
Mark Steve Samson <hello@marksteve.com>
2016-06-07 12:17:20 +0800

    Update templates and make things partially work

--- web/models/form.ex
+++ web/models/form.ex
@@ -1,14 +1,31 @@
+defmodule Formic.Field do
+  use Formic.Web, :model
+
+  embedded_schema do
+    field :name
+    field :type
+  end
+
+  @required_fields ~w(name type)
+  @optional_fields ~w()
+
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
+  end
+end
+
 defmodule Formic.Form do
   use Formic.Web, :model
 
   schema "forms" do
-    field :name, :string
+    field :name
     embeds_many :fields, Formic.Field
     timestamps
   end
 
   @required_fields ~w(name)
-  @optional_fields ~w()
+  @optional_fields ~w(fields)
 
   @doc """
   Creates a changeset based on the `model` and `params`.
@@ -19,14 +36,5 @@ defmodule Formic.Form do
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
-  end
-end
-
-defmodule Formic.Field do
-  use Ecto.Model
-
-  embedded_schema do
-    field :name
-    field :type
   end
 end
--- web/static/js/app.js
+++ web/static/js/app.js
@@ -2,15 +2,15 @@ import 'phoenix_html'
 import 'blissfuljs'
 import onmount from 'onmount'
 
-function createField() {
+function createField(i) {
   return {tag: 'p', className: 'flex', contents: [
     {tag: 'label', className: 'flex-auto mr2', contents: [
       {tag: 'span', textContent: 'Field name'},
-      {tag: 'input', name: 'field_name[]', placeholder: 'Meal'},
+      {tag: 'input', name: `form[fields][${i}][name]`, placeholder: 'Meal'},
     ]},
-    {tag: 'label', className: 'flex-auto', contents: [
+    {tag: 'label', className: 'col-4', contents: [
       {tag: 'span', textContent: 'Field type'},
-      {tag: 'select', name: 'field_type[]', contents: [
+      {tag: 'select', name: `form[fields][${i}][type]`, contents: [
         {tag: 'option', value: 'input', textContent: 'Input'},
         {tag: 'option', value: 'textarea', textContent: 'Textarea'},
         {tag: 'option', value: 'radio', textContent: 'Radio'},
@@ -23,13 +23,17 @@ function createField() {
 
 document.addEventListener('DOMContentLoaded', function () {
   onmount('[role=create-form]', function () {
+    let i = 0
     const formFields = $('[role=form-fields]', this)
     const addField = (e) => {
       e && e.preventDefault()
-      formFields._.contents([createField()])
+      formFields._.contents([createField(i)])
+      i++
     }
     $('[role=add-field]', this)._.events({click: addField})
-    addField()
+    if (!formFields.children.length) {
+      addField()
+    }
   })
   onmount()
 })
